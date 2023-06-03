d418bd302570dc64da6e63021f976773177a3f1e
Jeremiah <jeremiah.d.wise@gmail.com>
2023-06-03 18:24:23 -0500

    add generate token to utils folder

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler"
 
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // @ desc Auth user/set token
@@ -30,6 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
   })
 
   if(user) {
+    generateToken(res, user._id)
     res.status(201).json({
       _id: user._id,
       name: user.name,
