b9e17b80797520b403b04a8530ae0e0ef30b6db8
Jeremiah <jeremiah.d.wise@gmail.com>
2023-06-03 18:08:36 -0500

    create user model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 import asyncHandler from "express-async-handler"
 
+import User from '../models/userModel.js'
+
 // @ desc Auth user/set token
 // POST /api/users/auth
 // @access Public
@@ -11,7 +13,32 @@ const authUser = asyncHandler(async (req, res) => {
 // POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({message: 'Register User'})
+  //req.body is the data sent in the http body
+  const {name, email, password} = req.body
+  
+  const userExists = await User.findOne({email})
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exist')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if(user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @ desc Logout user
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
+connectDB()
+
+
 const port = process.env.PORT || 5000
 
 const app = express()
+
+// allows us to parse raw json
+app.use(express.json())
+// allows us to send form data
+app.use(express.urlencoded({extended: true}))
 
 app.use('/api/users', userRoutes)
 
